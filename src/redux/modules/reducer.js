2445d48c33a48a980ad48df226122df22c8b4186
yangtheng <yangtheng92@gmail.com>
2020-01-29 02:21:19 +0800

    set up roomInfo reducer structure

--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import roomInfo from './roomInfo';
-import userInfo from './userInfo';
 
 export default combineReducers({
   roomInfo,
-  userInfo,
 });
--- src/redux/modules/roomInfo.js
+++ src/redux/modules/roomInfo.js
@@ -1,9 +1,16 @@
+import io from "socket.io-client";
+const ENDPOINT = 'localhost:3001';
+
+const REQ_USER_JOIN = 'roomInfo/REQ_USER_JOIN';
+const REC_USER_JOIN = 'roomInfo/REC_USER_JOIN';
+
+let socket = io(ENDPOINT);
+
 const defaultState = {
   roomId: '',
+  users: [],
+  user: {},
 };
-
-// Define action names here
-
 
 // Reducer
 export default (state = defaultState, action) => {
@@ -14,5 +21,19 @@ export default (state = defaultState, action) => {
 };
 
 // export action creators
+const reqUserJoin = (user, roomId) => ({
+  type: REQ_USER_JOIN,
+  user,
+  roomId,
+});
 
-// export thunk actions
+const recUserJoin = users => ({
+  type: REC_USER_JOIN,
+  users,
+});
+
+export const userJoin = (user, roomId) => {
+  // socket emit
+  reqUserJoin(user, roomId);
+  // socket on
+}
