f3f649f75b0c391ff3ae5341ed96c8f07a4e7d45
yangtheng <yangtheng92@gmail.com>
2020-02-10 12:08:01 +0800

    Updates

---
+++ precache-manifest.0fc3c5c718178251acbfddb052814cd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3957f969eb5b298ea8e7bc91319a48f",
+    "url": "/landlord-frontend/index.html"
+  },
+  {
+    "revision": "201c93c8cee82d1fd97e",
+    "url": "/landlord-frontend/static/css/main.7e25baeb.chunk.css"
+  },
+  {
+    "revision": "5cdf63ef2fadbecbd656",
+    "url": "/landlord-frontend/static/js/2.1584e6f0.chunk.js"
+  },
+  {
+    "revision": "138a6d51240ab070e8d1b20c54be6394",
+    "url": "/landlord-frontend/static/js/2.1584e6f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "201c93c8cee82d1fd97e",
+    "url": "/landlord-frontend/static/js/main.0dadda07.chunk.js"
+  },
+  {
+    "revision": "a90d0694bba310eeeb96",
+    "url": "/landlord-frontend/static/js/runtime-main.628d177a.js"
+  }
+]);
